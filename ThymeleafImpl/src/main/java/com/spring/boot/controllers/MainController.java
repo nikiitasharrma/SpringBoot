1739111b84b8180abe7a025681929f426b577cc4
nikiitasharrma <nikita16301@gmail.com>
2024-03-06 18:05:59 +0530

    Iterations with thymeleaf

--- ThymeleafImpl/src/main/java/com/spring/boot/controllers/MainController.java
+++ ThymeleafImpl/src/main/java/com/spring/boot/controllers/MainController.java
@@ -1,6 +1,7 @@
 package com.spring.boot.controllers;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +13,20 @@ public class MainController {
 	@RequestMapping("/about")
 	public ModelAndView aboutHAndler() {
 		
-		ModelAndView mv = new ModelAndView();
-		
+		ModelAndView mv = new ModelAndView();	
 		mv.addObject("name", "Nikita Sharma");
-		
 		mv.addObject("curr_date", new Date().toLocaleString());
-		
 		mv.setViewName("about");
-		
+	
+		return mv;
+	}
+	
+	@RequestMapping("/iterate")
+	public ModelAndView iterationHandler() {
+		ModelAndView mv = new ModelAndView();
+		List<String> list = List.of("Ankit", "Bhuvan", "Chirag", "Dhruv", "Ethan");
+		mv.addObject("names", list);
+		mv.setViewName("iterator");
 		return mv;
 	}
 }
